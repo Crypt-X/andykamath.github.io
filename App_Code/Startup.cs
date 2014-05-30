42135220daa4e0f35d37b588c930c5df773e59ba
akamath426 <akamath426@gmail.com>
2014-05-30 19:27:00 -0400

    Pretty much nothing

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(andykamath.com.Startup))]
+namespace andykamath.com
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
